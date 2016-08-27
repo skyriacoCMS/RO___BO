c9bc1516b7a6837ff9a9ba0a69199090212d436a
skyriaco <savvasphy@gmail.com>
2016-08-27 19:49:06 +0200

    robobobobobob

---
+++ robo.py
@@ -0,0 +1,19 @@
+import time
+import sys
+
+for i in range(1,4):
+ ll = i%2
+ fname="robo"+str(ll)+".txt"
+ f1 = open(fname,"rb")
+ for line in f1: 
+    k = line.rstrip()
+    str(k)
+    print (k)    #, end='\r')
+
+
+ for j in range(0,15):
+    if(i != 3):
+        sys.stdout.write("\033[F")
+
+ time.sleep(1) 
+
